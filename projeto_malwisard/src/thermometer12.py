bcc176369044539fe349bb17d55f41e85f2a1ad7
Pedro Teixeira <pedroteixeir@poli.ufrj.br>
2025-11-26 11:15:11 -0300

    feat: added for loop in thermometer12.py

--- projeto_malwisard/src/thermometer12.py
+++ projeto_malwisard/src/thermometer12.py
@@ -1,4 +1,16 @@
 # =========================================================================================================#
+# ======================================== IMPORT MODULES =================================================#
+# =========================================================================================================#
+print("Importing modules...")
+
+import numpy as np
+import pickle
+import multiprocessing
+import cv2
+from sklearn.preprocessing import binarize
+
+print("Done!")
+# =========================================================================================================#
 # ====================================== EXPERIMENT PARAMETERS ============================================#
 # =========================================================================================================#
 
@@ -22,104 +34,92 @@ N = 12
 
 circularThermometer = False
 nBits = 4
+for i in range(0,10000,1000):
+    # ==================================== #
+    # DEBUG OPTIONS
+    inicio = i
+    fim = i + 1000 if i + 1000 < 9101 else 9100
+    tamanho = fim-inicio
 
-# ==================================== #
-# DEBUG OPTIONS
-inicio = 9000
-fim = 9100
-tamanho = fim-inicio
+    # ==================================== #
+    # FILE TO SAVE
+    filename = 'thermometer12_' + str(inicio) + '_' + str(fim-1)
 
-# ==================================== #
-# FILE TO SAVE
-filename = 'thermometer12_' + str(inicio) + '_' + str(fim-1)
 
-# =========================================================================================================#
-# ======================================== IMPORT MODULES =================================================#
-# =========================================================================================================#
-print("Importing modules...")
+    # =========================================================================================================#
+    # ====================================== IMPORT DATA ======================================================#
+    # =========================================================================================================#
+    print("Importing data...")
 
-import numpy as np
-import pickle
-import multiprocessing
-import cv2
-from sklearn.preprocessing import binarize
+    data = pickle.load( open( "data.p", "rb" ) )
+    if binaryClassification:
+        y = pickle.load( open( "y2.p", "rb" ) )
+    if multiClassification:
+        y = pickle.load( open( "y26.p", "rb" ) )
 
-print("Done!")
-
-# =========================================================================================================#
-# ====================================== IMPORT DATA ======================================================#
-# =========================================================================================================#
-print("Importing data...")
-
-data = pickle.load( open( "data.p", "rb" ) )
-if binaryClassification:
-    y = pickle.load( open( "y2.p", "rb" ) )
-if multiClassification:
-    y = pickle.load( open( "y26.p", "rb" ) )
-
-#y_6 = pickle.load( open( "y6.p", "rb" ) )
-print("Done!")
-
-data = data[inicio:fim]
-y = y[inicio:fim]
-
-# =========================================================================================================#
-# ========================================== INTERPOLATION ================================================#
-# =========================================================================================================#
-if interpolate:
-    print("Starting interpolation...")
-    new_data = []
-
-    for i in range(len(data)):
-        new_data.append(cv2.resize(data[i], dsize=(FACTOR, FACTOR), interpolation=cv2.INTER_CUBIC))
-    data = new_data
+    #y_6 = pickle.load( open( "y6.p", "rb" ) )
     print("Done!")
 
-# =========================================================================================================#
-# ========================================== RESHAPING ====================================================#
-# =========================================================================================================#
-print("Reshaping...")   
-data = np.array(data).reshape(tamanho, 224*224*3)
-print("Done!")
+    data = data[inicio:fim]
+    y = y[inicio:fim]
 
-# =========================================================================================================#
-# ========================================= BINARIZATION ==================================================#
-# =========================================================================================================#
-print("Binarization...")
+    # =========================================================================================================#
+    # ========================================== INTERPOLATION ================================================#
+    # =========================================================================================================#
+    if interpolate:
+        print("Starting interpolation...")
+        new_data = []
 
-if simple:
-    X = binarize(data, threshold = THRESHOLD)
+        for i in range(len(data)):
+            new_data.append(cv2.resize(data[i], dsize=(FACTOR, FACTOR), interpolation=cv2.INTER_CUBIC))
+        data = new_data
+        print("Done!")
 
-if dynamic:
-    for i in range(len(data)):
-        med = np.median(data[i])
-        data[i] = np.where(data[i] > med, 1, 0)
-    X = data
+    # =========================================================================================================#
+    # ========================================== RESHAPING ====================================================#
+    # =========================================================================================================#
+    print("Reshaping...")   
+    data = np.array(data).reshape(tamanho, 224*224*3)
+    print("Done!")
 
-if thermometer:
-    X = [[0 for i in range(224*224*N)] for j in range(tamanho)]
+    # =========================================================================================================#
+    # ========================================= BINARIZATION ==================================================#
+    # =========================================================================================================#
+    print("Binarization...")
 
-    for j in range(tamanho):
-        print(j)
-        for i in range(224*224):
-            for k in range(N):
-                if data[j][i] >= k*256/N and data[j][i] < (k+1)*256/N:
-                    X[j][N*i+k] = 1
-                    break
+    if simple:
+        X = binarize(data, threshold = THRESHOLD)
 
-if circularThermometer:
-    X = [[0 for i in range(224*224*nBits)] for j in range(tamanho)]
+    if dynamic:
+        for i in range(len(data)):
+            med = np.median(data[i])
+            data[i] = np.where(data[i] > med, 1, 0)
+        X = data
 
-    for j in range(tamanho):
-        print(j)
-        for i in range(224*224):
-            for k in range(nBits):
-                if data[j][i] >= k*256/nBits and data[j][i] <= (k+1)*256/nBits:
-                    for l in range(int(nBits/2)):
-                        X[j][nBits*i + (k+l) % nBits] = 1
-                    break
+    if thermometer:
+        X = [[0 for i in range(224*224*N)] for j in range(tamanho)]
 
-pickle.dump ( X, open( filename + "X.p", "wb" ) )
-pickle.dump ( y, open( filename + "y.p", "wb" ) )
+        for j in range(tamanho):
+            # print(j)
+            for i in range(224*224):
+                for k in range(N):
+                    if data[j][i] >= k*256/N and data[j][i] < (k+1)*256/N:
+                        X[j][N*i+k] = 1
+                        break
 
-print("Done")
+    if circularThermometer:
+        X = [[0 for i in range(224*224*nBits)] for j in range(tamanho)]
+
+        for j in range(tamanho):
+            print(j)
+            for i in range(224*224):
+                for k in range(nBits):
+                    if data[j][i] >= k*256/nBits and data[j][i] <= (k+1)*256/nBits:
+                        for l in range(int(nBits/2)):
+                            X[j][nBits*i + (k+l) % nBits] = 1
+                        break
+
+    pickle.dump ( X, open( filename + "X.p", "wb" ) )
+    pickle.dump ( y, open( filename + "y.p", "wb" ) )
+
+    print("Done")
